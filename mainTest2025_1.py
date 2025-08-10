d66b93cbe9e5ffd92809165da080de5097b2cc20
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2025-08-10 13:35:51 -0400

    1st commit

---
+++ mainTest2025_1.py
@@ -0,0 +1,8 @@
+# easy py program to greet a user
+
+def greetings_user():
+    name = input("What is your name? ")
+    print(f"Hi, {name}! Welome to a program.")
+
+# call said function
+greetings_user()
