54efdf9608fd2b80f8963629dd80c6a9bc23252a
ilya segal <65099651+ilyaVenom@users.noreply.github.com>
2025-08-10 13:54:17 -0400

    lesson 1 review pizza amts

---
+++ python_lessons_and_projects2025/lesson1Pizza.py
@@ -0,0 +1,8 @@
+#another lesson 0:
+PizzaSlices = "pizza slices"
+price = 5.99
+num_of_food = int(input(f"How many {PizzaSlices} are there?: "))
+
+total = num_of_food * price
+
+print (f"Your total is ${total}")
